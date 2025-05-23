63becb41245ce3adc1640eb0b1b8644d7a5bba5f
pichuck <kadavi9945@gmail.com>
2025-05-23 08:53:26 +0700

    commit pertama#1 cui

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+// Route untuk halaman lain yang kamu butuhkan
